b15a806f60583468235917329d904da7cbf8be15
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-04-09 23:47:37 -0400

    finalized

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         String cmd = "";
         Scanner console = new Scanner(System.in);
 
-
         do {
             //display list
             displayList();
@@ -22,40 +21,48 @@ public class Main {
             switch (cmd)
             {
                 case "A":
-                    //prompt the user for a list item
-                    //make sure it is not an empty string using method
-                    //add it to the list
+                    addToList(console, list);
                     break;
                 case "D":
-                    //prompt user for number of item to delete
-                    //translate the number to an index by subtracting 1
-                    //remove item from the list
+                    deleteFromList(console, list);
                     break;
                 case "P":
-
+                    printMenu(list);
                     break;
                 case "Q":
-                    System.exit(0);
+                    if (SafeInput.getYNConfirm(console, "Are you sure")){
+                        done =true;
+                    }
+                    System.exit(0); //quits
                     break;
+
 
             }
             System.out.println("cmd is " + cmd);
-
-
         }
         while (!done);
-
     }
-
+    public static void addToList(Scanner in, ArrayList list){
+        String objectToAdd = SafeInput.getNonZeroLenString(in, "Enter the item to add to the array list" );
+        list.add(objectToAdd); //adds object to list
+    }
+    public static void  deleteFromList(Scanner in, ArrayList list ){
+        int objectToDelete = SafeInput.getRangedInt(in, "Enter the number of the item to delete", 1, list.size());
+        list.remove(objectToDelete-1); //removes object based on base 1 index
+    }
+    public static void printMenu(ArrayList list){
+        for (int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
     private static void displayList() {
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
         if (list.size() != 0) {
             for (int i = 0; i < list.size(); i++) {
-                System.out.printf("%3d%40s", i + 1, list.get(i)); //for index based 1
+                System.out.printf("%d. %s\n", i + 1, list.get(i)); //for index based 1
             }
         }
         else
             System.out.println("++++          List is empty           ++++");
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("");
     }
 }
